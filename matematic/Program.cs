96106f1593daea75f769360029741a028418dcc8
qv4sOK <mobail3c@gmail.com>
2022-03-27 23:49:51 +0300

    Добавьте файлы проекта.

---
+++ matematic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matematic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число с точкой: ");
+            // user input = Convert.ToDuble(Console.ReadLine());
+
+            float user_input = float.Parse(Console.ReadLine());
+
+            float result;
+
+            result = user_input + 10f;
+
+            result += 10;
+        }
+    }
+}
